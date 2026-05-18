2915d067cf7170897fb0846237c2474ea7dd0276
AinaMisaela98 <146171231+AinaMisaela98@users.noreply.github.com>
2026-05-18 19:36:23 +0200

    add admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,34 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // 👑 ADMIN
         User::updateOrCreate(
             ['email' => 'admin@gmail.com'],
             [
                 'name' => 'Admin',
-                'password' => Hash::make('admin123456'),
+                'password' => Hash::make('12345678'),
                 'role' => 'admin',
-            ]
-        );
-
-        // 👤 TEST USER
-        User::updateOrCreate(
-            ['email' => 'test@example.com'],
-            [
-                'name' => 'Test User',
-                'password' => Hash::make('password123'),
-                'role' => 'user',
             ]
         );
     }
